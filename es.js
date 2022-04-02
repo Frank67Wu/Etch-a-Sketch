a5e522c50829c5055e2e91ed16c46ff94a97621d
Frank67Wu <f67wu@uwaterloo.ca>
2022-04-02 05:07:05 -0400

    Add resetGrid button

--- es.js
+++ es.js
@@ -14,18 +14,42 @@ function createGrid(n) {
             block.style.width = 960/n + "px";
             block.style.height = 960/n + "px";
             block.addEventListener('mouseenter', e => {
-                block.style.backgroundColor = 'lightblue';
+                block.style.backgroundColor = 'black';
             })
     
             row.appendChild(block);
         }
-        document.body.appendChild(row);
+        let myContainer = document.getElementsByClassName('container')[0];
+        myContainer.appendChild(row);
     }
 }
 
 
+function removeGrid() {
+    let myContainer = document.getElementsByClassName('container')[0];
+    for (let i = 0; i  < myContainer.children.length; i++) {
+        myContainer.remove(myContainer.firstChild);
+    }   
+    let newContainer = document.createElement('div');
+    newContainer.classList.add('container');
+    document.body.appendChild(newContainer);
+}
 
-createGrid(10);
+function userPrompt() {
+    removeGrid();
+    let a = prompt();
+    if (a <= 100) {
+        createGrid(a);
+    }
+    else {
+        a = prompt();
+    } 
+}
+
+let resetButton = document.body.getElementsByTagName('button')[0];
+resetButton.addEventListener('click', e => {
+    userPrompt();
+})
 
 
 
